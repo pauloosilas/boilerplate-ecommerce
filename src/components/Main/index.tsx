5446d7afb866adc552dd5a7783e8841c22633c8a
pauloosilas <paulo3356@gmail.com>
2023-06-17 15:58:12 -0300

    add styles

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,15 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Imagem de um átomo ao lado." />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>Typescript, NextJS, e Styled Components</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Desenvolvedor de frente para um computador"
+    />
+  </S.Wrapper>
 )
 
 export default Main
